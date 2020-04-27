ee08e233516ad46f2339063fc6c89b6356ca86ed
markbasser <38104324+markbasser@users.noreply.github.com>
2020-04-27 21:33:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -34,39 +34,39 @@ async def on_message(message):
         # チャンネルへメッセージを送信
         await message.channel.send(f"{message.author.mention}さん Good Night! Go to bed early♡")  # f文字列（フォーマット済み文字列リテラル）
 
-    if message.content == "Hllo All":
+    if message.content == "Hello All":
         # チャンネルへメッセージを送信
         await message.channel.send(f"{message.author.mention}さん ☆おはようございます☆")  # f文字列（フォーマット済み文字列リテラル）
         
-    if message.content == "/tip bgpt 500 @༺.BEGPT.༻":
+    if message.content == "/tip bgpt 500 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip ben 50 {message.author.mention}  Swapped from BGPT500<:BGPT02:698471366004965406> to BEN500<:BENKEICOIN04:698471407650209832> .　" )  # f文字列（フォーマット済み文字列リテラル）
 
-    if message.content == "/tip bgpt 1000 @༺.BEGPT.༻":
+    if message.content == "/tip bgpt 1000 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip ben 100 {message.author.mention}  Swapped from BGPT1000<:BGPT02:698471366004965406> to BEN100<:BENKEICOIN04:698471407650209832>")  # f文字列（フォーマット済み文字列リテラル）
  
-    if message.content == "/tip ben 100 @༺.BEGPT.༻":
+    if message.content == "/tip ben 100 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip bgpt 1000 {message.author.mention}  Swapped from BEN100<:BENKEICOIN04:698471407650209832> to BGPT1000<:BGPT02:698471366004965406> ")  # f文字列（フォーマット済み文字列リテラル）
 
-    if message.content == "/tip ben 500 @༺.BEGPT.༻":
+    if message.content == "/tip ben 500 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip bgpt 5000 {message.author.mention}  Swapped from BEN100<:BENKEICOIN04:698471407650209832> to BGPT1000<:BGPT02:698471366004965406> ")  # f文字列（フォーマット済み文字列リテラル）
 
-    if message.content == "/tip BGPT 500 @༺.BEGPT.༻":
+    if message.content == "/tip BGPT 500 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip ben 50 {message.author.mention}  Swapped from BGPT500<:BGPT02:698471366004965406> to BEN500<:BENKEICOIN04:698471407650209832> .　" )  # f文字列（フォーマット済み文字列リテラル）
 
-    if message.content == "/tip BGPT 1000 @༺.BEGPT.༻":
+    if message.content == "/tip BGPT 1000 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip ben 100 {message.author.mention}  Swapped from BGPT1000<:BGPT02:698471366004965406> to BEN100<:BENKEICOIN04:698471407650209832>")  # f文字列（フォーマット済み文字列リテラル）
  
-    if message.content == "/tip BEN 100 @༺.BEGPT.༻":
+    if message.content == "/tip BEN 100 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip bgpt 1000 {message.author.mention}  Swapped from BEN100<:BENKEICOIN04:698471407650209832> to BGPT1000<:BGPT02:698471366004965406> ")  # f文字列（フォーマット済み文字列リテラル）
 
-    if message.content == "/tip BEN 500 @༺.BEGPT.༻":
+    if message.content == "/tip BEN 500 <@&700145429647720540>":
         # チャンネルへメッセージを送信
         await message.channel.send(f"/tip bgpt 5000 {message.author.mention}  Swapped from BEN100<:BENKEICOIN04:698471407650209832> to BGPT1000<:BGPT02:698471366004965406> ")  # f文字列（フォーマット済み文字列リテラル）
 
